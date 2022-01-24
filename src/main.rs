a94d157cbc5516f5519cf3c6367ea88ad5c96121
Carlos Macasaet <github@carlos-m.net>
2022-01-23 21:56:08 -0800

    Use fixed-precision decimals and simplify code

--- src/main.rs
+++ src/main.rs
@@ -22,13 +22,14 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
 
-use std::cmp::Ordering;
+use clap::Parser;
+use rust_decimal::Decimal;
+use rust_decimal_macros::dec;
 use std::collections::{HashMap, HashSet};
 use std::fs::File;
 use std::io::stdout;
 use std::io::Write;
 use std::io::{BufRead, BufReader};
-use clap::Parser;
 
 /// Determine how likely a word is to narrow the possible target words
 ///
@@ -39,9 +40,9 @@ use clap::Parser;
 ///
 /// Return: A value greater than or equal to 1.0. The higher the number, the more likely it is to
 ///         narrow the list of possible target words.
-fn score(word: &str, scores: &HashMap<char, f32>) -> f32 {
+fn score(word: &str, scores: &HashMap<char, Decimal>) -> Decimal {
     let chars = word.chars().collect::<HashSet<char>>();
-    let mut result = 0f32;
+    let mut result = dec!(0.00);
     for c in chars {
         result += scores[&c];
     }
@@ -61,33 +62,33 @@ struct Arguments {
 fn main() {
     let arguments = Arguments::parse();
     // letter frequencies from: https://www3.nd.edu/~busiforc/handouts/cryptography/letterfrequencies.html
-    let letter_scores: HashMap<char, f32> = HashMap::from([
-        ('e', 56.88),
-        ('a', 43.31),
-        ('r', 38.64),
-        ('i', 38.45),
-        ('o', 36.51),
-        ('t', 35.43),
-        ('n', 33.92),
-        ('s', 29.23),
-        ('l', 27.98),
-        ('c', 23.13),
-        ('u', 18.51),
-        ('d', 17.25),
-        ('p', 16.14),
-        ('m', 15.36),
-        ('h', 15.31),
-        ('g', 12.59),
-        ('b', 10.56),
-        ('f', 9.24),
-        ('y', 9.06),
-        ('w', 6.57),
-        ('k', 5.61),
-        ('v', 5.13),
-        ('x', 1.48),
-        ('z', 1.39),
-        ('j', 1.00),
-        ('q', 1.00),
+    let letter_scores: HashMap<char, Decimal> = HashMap::from([
+        ('e', dec!(56.88)),
+        ('a', dec!(43.31)),
+        ('r', dec!(38.64)),
+        ('i', dec!(38.45)),
+        ('o', dec!(36.51)),
+        ('t', dec!(35.43)),
+        ('n', dec!(33.92)),
+        ('s', dec!(29.23)),
+        ('l', dec!(27.98)),
+        ('c', dec!(23.13)),
+        ('u', dec!(18.51)),
+        ('d', dec!(17.25)),
+        ('p', dec!(16.14)),
+        ('m', dec!(15.36)),
+        ('h', dec!(15.31)),
+        ('g', dec!(12.59)),
+        ('b', dec!(10.56)),
+        ('f', dec!(9.24)),
+        ('y', dec!(9.06)),
+        ('w', dec!(6.57)),
+        ('k', dec!(5.61)),
+        ('v', dec!(5.13)),
+        ('x', dec!(1.48)),
+        ('z', dec!(1.39)),
+        ('j', dec!(1.00)),
+        ('q', dec!(1.00)),
     ]);
     let file = File::open(arguments.word_list.clone())
         .unwrap_or_else(|_| panic!("Unable to open dictionary: {}", arguments.word_list));
@@ -97,22 +98,13 @@ fn main() {
         .filter(|word| word.len() == arguments.length as usize)
         .filter(|word| word.to_lowercase().eq(word))
         .map(|word| (word.clone(), score(&word, &letter_scores)))
-        .collect::<Vec<(String, f32)>>();
-    candidates.sort_unstable_by(|x, y| -> Ordering {
-        if x.1 > y.1 {
-            Ordering::Less
-        } else if y.1 > x.1 {
-            Ordering::Greater
-        } else {
-            Ordering::Equal
-        }
-    });
+        .collect::<Vec<(String, Decimal)>>();
+    candidates.sort_unstable_by_key(|tuple| -tuple.1);
     let output = stdout();
     let mut output = output.lock();
     for candidate in candidates.iter() {
-        match writeln!(output, "{}: {}", candidate.1, candidate.0) {
-            Ok(_) => continue,
-            Err(_) => break,
+        if writeln!(output, "{}: {}", candidate.1, candidate.0).is_err() {
+            break;
         }
     }
 }
